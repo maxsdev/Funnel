3719724bfa801826e8a2930b33ac9df8212f1075
Сысоев Максим <m.sysoev@movavi.com>
2023-01-29 20:53:40 +0600

    Add tests.

--- app/src/test/java/ru/maxsdev/funnel/main/MainViewModelTest.kt
+++ app/src/test/java/ru/maxsdev/funnel/main/MainViewModelTest.kt
@@ -23,21 +23,58 @@ internal class MainViewModelTest {
 
     @Test
     fun test_GreetingsViewState_10_00_10_Jan_2023() {
-        val lifecycle = LifecycleRegistry(mock(LifecycleOwner::class.java))
-        lifecycle.markState(Lifecycle.State.RESUMED)
-        `when`(lifecycleOwner.lifecycle).thenReturn(lifecycle)
-
-        val dateProvider: DateProvider = object : DateProvider {
-            private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
-            override val date: LocalDateTime
-                get() = LocalDateTime.parse("2023-01-10T10:00:00.0", formatter)
-        }
-        val viewModel = MainViewModel(dateProvider)
-
+        mockLifecycle()
+        val viewModel = MainViewModel(createDateProvider("2023-01-10T10:00:00.0"))
         viewModel.greetingsViewState
             .observe(lifecycleOwner) { viewState ->
                 viewState.date.assert("TUESDAY, 10 JANUARY")
                 viewState.greetings.assert(R.string.header_good_morning)
             }
+    }
+
+    @Test
+    fun test_GreetingsViewState_15_00_21_Oct_2023() {
+        mockLifecycle()
+        val viewModel = MainViewModel(createDateProvider("2023-10-21T15:00:00.0"))
+        viewModel.greetingsViewState
+            .observe(lifecycleOwner) { viewState ->
+                viewState.date.assert("SATURDAY, 21 OCTOBER")
+                viewState.greetings.assert(R.string.header_good_afternoon)
+            }
+    }
+
+    @Test
+    fun test_GreetingsViewState_2_00_12_Oct_2023() {
+        mockLifecycle()
+        val viewModel = MainViewModel(createDateProvider("2023-10-12T02:00:00.0"))
+        viewModel.greetingsViewState
+            .observe(lifecycleOwner) { viewState ->
+                viewState.date.assert("THURSDAY, 12 OCTOBER")
+                viewState.greetings.assert(R.string.header_good_night)
+            }
+    }
+
+    @Test
+    fun test_GreetingsViewState_21_30_14_Aug_2023() {
+        mockLifecycle()
+        val viewModel = MainViewModel(createDateProvider("2023-08-14T21:30:00.0"))
+        viewModel.greetingsViewState
+            .observe(lifecycleOwner) { viewState ->
+                viewState.date.assert("MONDAY, 14 AUGUST")
+                viewState.greetings.assert(R.string.header_good_evening)
+            }
+    }
+
+    private fun createDateProvider(pattern: String): DateProvider =
+        object : DateProvider {
+            private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+            override val date: LocalDateTime
+                get() = LocalDateTime.parse(pattern, formatter)
+        }
+
+    private fun mockLifecycle() {
+        val lifecycle = LifecycleRegistry(mock(LifecycleOwner::class.java))
+        lifecycle.markState(Lifecycle.State.RESUMED)
+        `when`(lifecycleOwner.lifecycle).thenReturn(lifecycle)
     }
 }
